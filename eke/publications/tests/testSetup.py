e3235f8b5c897ad3bb44750bc70c515e637e4ed3
Sean Kelly <sean.c.kelly@me.com>
2011-07-17 23:08:04 +0000

    Add unit tests for monkeypatches, facets

--- eke/publications/tests/testSetup.py
+++ eke/publications/tests/testSetup.py
@@ -10,6 +10,8 @@ from eke.publications.tests.base import BaseTestCase
 from Products.CMFCore.utils import getToolByName
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
+from eea.facetednavigation.interfaces import IPossibleFacetedNavigable
+from eke.publications.content.publicationfolder import PublicationFolder
 import unittest
 
 class SetupTest(BaseTestCase):
@@ -33,11 +35,15 @@ class SetupTest(BaseTestCase):
         )
         for v in vocabs:
             vocab = queryUtility(IVocabularyFactory, name=v)
-            self.failIf(v is None, u'Vocabulary %s not available' % v)
-    # TBD:
-    # def testMonkeyPatching(self):
-    #     '''Check that the Plone portal has a "macros" attribute to work around the eea.facetedvocabularies problem'''
-        
+            self.failIf(vocab is None, u'Vocabulary %s not available' % v)
+    def testMonkeyPatching(self):
+        '''Check that the Plone portal has a "macros" attribute to work around the eea.facetedvocabularies problem'''
+        from Products.CMFPlone.Portal import PloneSite
+        viewFunction = PloneSite.view
+        self.failUnless(hasattr(viewFunction, 'macros'), 'PloneSite.view not monkeypatched with a "macros" attribute')
+    def testFacetNavigability(self):
+        '''Ensure the Publications Folder type can use faceted navigation.'''
+        self.failUnless(IPossibleFacetedNavigable.implementedBy(PublicationFolder))
 
 def test_suite():
     suite = unittest.TestSuite()
