aefdd7829cf1df9835ae3d61267fcdc89eda0c50
Sean Kelly <sean.c.kelly@me.com>
2011-07-17 23:06:27 +0000

    Make it possible to use faceted nav on objects of pub folder; add event handler to config faceted nav on creation

--- eke/publications/content/publicationfolder.py
+++ eke/publications/content/publicationfolder.py
@@ -4,12 +4,15 @@
 
 '''Publication folder.'''
 
+from eea.facetednavigation.interfaces import IPossibleFacetedNavigable
 from eke.knowledge.content import knowledgefolder
 from eke.publications import ProjectMessageFactory as _
 from eke.publications.config import PROJECTNAME
 from eke.publications.interfaces import IPublicationFolder
+from eke.publications.utils import setFacetedNavigation
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
 
 PublicationFolderSchema = knowledgefolder.KnowledgeFolderSchema.copy() + atapi.Schema((
@@ -28,10 +31,18 @@ finalizeATCTSchema(PublicationFolderSchema, folderish=True, moveDiscussion=False
 
 class PublicationFolder(knowledgefolder.KnowledgeFolder):
     '''Publication folder which contains publications.'''
-    implements(IPublicationFolder)
+    implements(IPublicationFolder, IPossibleFacetedNavigable)
     portal_type               = 'Publication Folder'
     _at_rename_after_creation = True
     schema                    = PublicationFolderSchema
     additionalDataSources     = atapi.ATFieldProperty('additionalDataSources')
 
 atapi.registerType(PublicationFolder, PROJECTNAME)
+
+def addFacetedNavigation(obj, event):
+    '''Set up faceted navigation on all newly created Publication Folders.'''
+    if not IPublicationFolder.providedBy(obj): return
+    factory = getToolByName(obj, 'portal_factory')
+    if factory.isTemporary(obj): return
+    request = obj.REQUEST
+    setFacetedNavigation(obj, request)
