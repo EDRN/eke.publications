129cfe9d2cb1f8b45f5ba3a274eb68617c9c3a95
Sean Kelly <sean.c.kelly@me.com>
2011-07-27 02:45:02 +0000

    Inspire lofty software proclamations via micro-level numbering stimulus

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os.path
 # ------------
 
 _name        = 'eke.publications'
-_version     = '1.1.4'
+_version     = '1.1.5'
 _description = 'Publications Management for the EDRN Knowledge Environment'
 _url         = 'http://cancer.jpl.nasa.gov/products/eke-publications'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/eke'
