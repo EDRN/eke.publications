e9db051de837e1fda67595b8952867126b5e4e36
Sean Kelly <sean.c.kelly@me.com>
2013-07-29 16:12:59 +0000

    Get rid of the test for the monkeypatch as there is no more monkeypatch

--- eke/publications/tests/testSetup.py
+++ eke/publications/tests/testSetup.py
@@ -22,11 +22,6 @@ class SetupTest(unittest.TestCase):
         indexes = catalog.indexes()
         for i in ('authors',):
             self.failUnless(i in indexes, '%s not found in catalog indexes' % i)
-    def testMonkeyPatching(self):
-        '''Check that the Plone portal has a "macros" attribute to work around the eea.facetedvocabularies problem'''
-        from Products.CMFPlone.Portal import PloneSite
-        viewFunction = PloneSite.view
-        self.failUnless(hasattr(viewFunction, 'macros'), 'PloneSite.view not monkeypatched with a "macros" attribute')
     def testFacetNavigability(self):
         '''Ensure the Publications Folder type can use faceted navigation.'''
         self.failUnless(IPossibleFacetedNavigable.implementedBy(PublicationFolder))
